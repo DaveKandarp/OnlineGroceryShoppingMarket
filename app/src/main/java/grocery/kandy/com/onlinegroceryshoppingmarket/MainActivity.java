45d9faa4aaa307f75918a16f19f10c2f6a464212
DaveKandarp <davekandarp1990@gmail.com>
2019-01-01 20:09:08 +0530

    Initial commit

---
+++ app/src/main/java/grocery/kandy/com/onlinegroceryshoppingmarket/MainActivity.java
@@ -0,0 +1,13 @@
+package grocery.kandy.com.onlinegroceryshoppingmarket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
